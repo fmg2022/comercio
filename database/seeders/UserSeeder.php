7e7e3ff770abc0c5076979d1fc3b18e2bd4cf754
Gerardo <fernandez.gerardo@gmail.com>
2025-08-07 20:58:53 -0300

    refactor(seed): add new user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,19 +13,27 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'name' => 'Admin Frame',
-            'surname' => 'Prime',
-            'phone' => '+54 1234567890',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('123456'),
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'Juan Carlos',
-            'surname' => 'Perez',
-            'phone' => '+54 0987654321',
-            'email' => 'juan@gmail.com',
-            'password' => bcrypt('123456'),
+            [
+                'name' => 'Admin Frame',
+                'surname' => 'Prime',
+                'phone' => '+54 1234567890',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Juan Carlos',
+                'surname' => 'Perez',
+                'phone' => '+54 0987654321',
+                'email' => 'juan@gmail.com',
+                'password' => bcrypt('123456'),
+            ],
+            [
+                'name' => 'Fernando',
+                'surname' => 'Suarez',
+                'phone' => '+54 0987654321',
+                'email' => 'fernando@gmail.com',
+                'password' => bcrypt('123456'),
+            ]
         ]);
     }
 }
