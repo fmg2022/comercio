c1fad4922ea82803ff67662310e097431da8f2f0
Gerardo <fernandez.gerardo@gmail.com>
2025-08-15 19:49:26 -0300

    fix(styles): resolve Tailwind class rendering issues
    - Implement static class array solution

--- resources/views/components/images/borderFill.blade.php
+++ resources/views/components/images/borderFill.blade.php
@@ -1,6 +1,11 @@
 @props(['color' => 'purple'])
 
-<div
-  class="p-5 mx-auto mb-4 bg-gradient-to-b from-{{ $color }}-800 to-{{ $color }}-300 border border-{{ $color }}-700 rounded-md">
+@php
+  $colorClasses = [
+      'purple' => 'from-purple-800 to-purple-300 border border-purple-700',
+  ];
+@endphp
+
+<div class="p-5 mx-auto mb-4 bg-gradient-to-b {{ $colorClasses[$color] }} rounded-md">
   <img {{ $attributes->merge(['class' => 'size-40 object-cover rounded-sm']) }}>
 </div>
