9b6aae199e0cae383d4459a7ecf4f9d3cd853009
Gerardo <fernandez.gerardo@gmail.com>
2025-08-20 20:20:56 -0300

    feat(modal): add tittleClass prop
    - Add tittleClass prop for custom class in h2 tag

--- resources/views/components/modals/simple.blade.php
+++ resources/views/components/modals/simple.blade.php
@@ -1,11 +1,11 @@
-@props(['title' => ''])
+@props(['title' => '', 'titleClass' => ''])
 
 <dialog
   {{ $attributes->merge(['class' => 'backdrop:bg-purple-900/35 backdrop:backdrop-blur-sm rounded-md overflow-hidden top-1/2 left-1/2 -translate-1/2']) }}
   closedby="any">
   <div class="relative flex flex-col gap-4 p-4">
     @if ($title)
-      <h2 class="mt-3 text-2xl text-center text-purple-900 font-semibold">{{ $title }}</h2>
+      <h2 class="mt-3 text-2xl text-center text-purple-900 font-semibold {{ $titleClass }}">{{ $title }}</h2>
     @endif
     <form method="dialog" class="fixed top-3 right-3 w-fit">
       <button class="p-1 text-slate-500 cursor-pointer">
