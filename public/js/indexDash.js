00a71f5ca1bf204d0161254ddd87646dff522346
Gerardo <fernandez.gerardo@gmail.com>
2026-04-27 17:34:54 -0300

    feat(dashboard): set max-height of data-target elements with target element heights
    Loop through [data-target] elements and set max-height to offsetHeight of matched objective element

--- public/js/indexDash.js
+++ public/js/indexDash.js
@@ -5,4 +5,14 @@ import toggleInputs from "./dashboard/toggleInputs.js"
 document.addEventListener('DOMContentLoaded', () => {
   toggleInputs('[name="toggle-btns"]')
   // asideMenu()
+
+  const targets = document.querySelectorAll('[data-target]')
+
+  if (targets.length <= 0) return;
+
+  targets.forEach(target => {
+    const objetive = document.querySelector(`[data-objetive="${target.dataset.target}"]`)
+
+    if (objetive) target.style.maxHeight = `${objetive.offsetHeight}px`;
+  })
 })
