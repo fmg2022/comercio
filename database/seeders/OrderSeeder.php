57f83841a97a2255cfad6a0f7005a38af1b57d5a
Gerardo <fernandez.gerardo@gmail.com>
2026-05-12 21:49:58 -0300

    refactor(order): optimize order for better performance

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -14,38 +14,38 @@ class OrderSeeder extends Seeder
 	public function run(): void
 	{
 		Order::factory(80)->create()->each(function ($order) {
+			$validOffers = DB::table('offer_product')
+				->join('offers', 'offer_product.offer_id', '=', 'offers.id')
+				->join('offer_templates', 'offers.offer_template_id', '=', 'offer_templates.id')
+				->join('offer_types', 'offer_templates.offer_type_id', '=', 'offer_types.id')
+				->whereDate('offers.end_date', '>=', $order->date)
+				->whereDate('offers.start_date', '<=', $order->date)
+				->get(['offer_product.product_id', 'offer_templates.id', 'offer_types.code', 'offer_templates.pay_qty', 'offer_templates.buy_qty'])
+				->groupBy('product_id');
+
 			$products = DB::table('products')->inRandomOrder()
 				->limit(rand(1, 8))
 				->get()
-				->mapWithKeys(function ($product) {
-					$offerData = DB::table('offer_product')
-						->join('offers', 'offer_product.offer_id', '=', 'offers.id')
-						->join('offer_templates', 'offers.offer_template_id', '=', 'offer_templates.id')
-						->join('offer_types', 'offer_templates.offer_type_id', '=', 'offer_types.id')
-						->where('offer_product.product_id', $product->id)
-						->select('offer_templates.id', 'offer_types.code', 'offer_templates.pay_qty', 'offer_templates.buy_qty')
-						->inRandomOrder()
-						->first();
+				->mapWithKeys(function ($product) use ($validOffers) {
+					$offerData = $validOffers->get($product->id)?->random();
 					$qty = rand(1, 6);
-					$isEmptyOffer = empty($offerData);
 
 					return [
 						$product->id => [
 							'quantity' => $qty,
 							'price' => $product->price,
-							'discount' => !$isEmptyOffer ? round($this->appyDiscount($offerData->code, $qty, $product->price, $offerData->pay_qty, $offerData->buy_qty), 2) : 0,
-							'offer_template_id' => !$isEmptyOffer ? $offerData->id : '',
-							'offer_type_code' => !$isEmptyOffer ? $offerData->code : '',
+							'discount' => $offerData ? round($this->applyDiscount($offerData->code, $qty, $product->price, $offerData->pay_qty, $offerData->buy_qty), 2) : 0,
+							'offer_template_id' => $offerData->id ?? '',
+							'offer_type_code' => $offerData->code ?? '',
 						]
 					];
-				})
-				->toArray();
+				});
 
-			$order->products()->attach($products);
+			$order->products()->attach($products->toArray());
 		});
 	}
 
-	private function appyDiscount(string $offerType, int $quantity, float $price, float $pay_qty, int $buy_qty): float
+	private function applyDiscount(string $offerType, int $quantity, float $price, float $pay_qty, int $buy_qty): float
 	{
 		if ($offerType === 'PERCENTAGE') {
 			return $price * $pay_qty * $quantity;
