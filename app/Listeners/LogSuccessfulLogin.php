35f9f0e31bea2537b303a47848a0cbbc7422e515
Gerardo <fernandez.gerardo@gmail.com>
2026-06-17 17:03:31 -0300

    style(listener): remove unused call class

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Models\UserSessionHistory;
 use Illuminate\Auth\Events\Login;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 
 class LogSuccessfulLogin
 {
