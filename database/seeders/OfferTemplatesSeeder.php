d10e643be45c4263d344ca91c16b9a9115751266
Gerardo <fernandez.gerardo@gmail.com>
2026-06-27 17:55:37 -0300

    feat(offer template): add 5 values for seeder

--- database/seeders/OfferTemplatesSeeder.php
+++ database/seeders/OfferTemplatesSeeder.php
@@ -48,6 +48,41 @@ class OfferTemplatesSeeder extends Seeder
                 'buy_qty' => 1,
                 'pay_qty' => 50,
             ],
+            [
+                'name' => 'Descuento 15%',
+                'description' => '15% de descuento en productos seleccionados',
+                'offer_type_id' => 1,
+                'buy_qty' => 1,
+                'pay_qty' => 0.15,
+            ],
+            [
+                'name' => 'Descuento de $500',
+                'description' => 'Descuento de $500 en productos seleccionados',
+                'offer_type_id' => 3,
+                'buy_qty' => 1,
+                'pay_qty' => 500,
+            ],
+            [
+                'name' => 'Descuento de $1000',
+                'description' => 'Descuento de $1000 en productos seleccionados',
+                'offer_type_id' => 3,
+                'buy_qty' => 1,
+                'pay_qty' => 1000,
+            ],
+            [
+                'name' => '3 x 1',
+                'description' => 'Lleva 3 productos por el precio de 1',
+                'offer_type_id' => 2,
+                'buy_qty' => 3,
+                'pay_qty' => 1,
+            ],
+            [
+                'name' => 'Descuento 30%',
+                'description' => '30% de descuento en productos seleccionados',
+                'offer_type_id' => 1,
+                'buy_qty' => 1,
+                'pay_qty' => 0.3,
+            ]
         ]);
     }
 }
