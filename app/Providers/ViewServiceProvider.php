9e4f2e7e2776300da19e997c706b1840382d5930
Gerardo <fernandez.gerardo@gmail.com>
2026-07-03 23:09:20 -0300

    feat(service): add cart value in ViewServiceProvider

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -23,7 +23,8 @@ class ViewServiceProvider extends ServiceProvider
         // sharing daa with multiple views
         View::composer(['pages.index', 'pages.home.product.show', 'pages.home.product.list', 'pages.home.cart.index'], function ($view) {
             $view->with('categories', \App\Models\Category::getFullTree())
-                ->with('offers', \App\Models\Offer::with(['offerTemplate:id,name,offer_type_id,buy_qty,pay_qty', 'offerTemplate.offerType:id,code'])->active()->get()->keyBy('id')->toArray());
+                ->with('offers', \App\Models\Offer::with(['offerTemplate:id,name,offer_type_id,buy_qty,pay_qty', 'offerTemplate.offerType:id,code'])->active()->get()->keyBy('id')->toArray())
+                ->with('cart', auth()->user()->cart ?? []);
         });
     }
 }
