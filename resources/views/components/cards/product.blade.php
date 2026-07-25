a7773a4f6099f671ab39e1f1064f20984c9f76f3
Gerardo <fernandez.gerardo@gmail.com>
2026-07-24 21:02:19 -0300

    refactor(product): enchance product card
    - Implement add-to-cart livewire component that remplaced old form to add product to cart.
    - Fix route for product show.
    - Enchance styles.
    - Implement styles for 'grilla' and 'list' view

--- resources/views/components/cards/product.blade.php
+++ resources/views/components/cards/product.blade.php
@@ -1,69 +1,21 @@
-@php
-  $offerID = $product->activeOffer();
-@endphp
-<article class="max-w-fit h-max pb-3 rounded-xl shadow-lg bg-slate-300 overflow-hidden">
-  <a href="{{ route('product.show', $product->id) }}"
-    class="relative hover:[&>span]:bg-slate-500 hover:[&>span]:sm:opacity-100">
+@php $offerID = $product->activeOffer(); @endphp
+<article
+  class="h-full flex rounded-xl bg-slate-300 shadow-lg overflow-hidden group-[.list]:max-w-full group-[.list]:w-full group-[.list]:p-0 group-[.list]:flex-row group-[.grilla]:max-w-fit group-[.grilla]:pb-3 group-[.grilla]:flex-col">
+  <a href="{{ route('products.show', $product->id) }}"
+    class="relative hover:[&>span]:bg-slate-500 hover:[&>span]:sm:opacity-100 group-[.list]:flex group-[.list]:max-w-56">
     <img class="aspect-square" src="{{ asset('images/products/' . $product->image) }}" alt="{{ $product->name }}"
       draggable="false" width="310">
     <span
-      class="absolute bottom-0 left-1/2 -translate-x-1/2 px-3 py-2 text-white bg-slate-600 rounded-lg sm:bottom-1/2 sm:translate-y-1/2 sm:opacity-0 transition-opacity duration-300">
+      class="absolute bottom-0 left-1/2 -translate-x-1/2 px-3 py-2 text-nowrap text-white bg-slate-600 rounded-lg sm:bottom-1/2 sm:translate-y-1/2 sm:opacity-0 transition-opacity duration-300">
       Ver Producto
     </span>
   </a>
-  <div class="px-6 py-4">
-    <h2 class="font-bold text-2xl mb-1 text-slate-700">{{ $product->brand->name }}</h2>
-    <p class="text-slate-500 text-base">{{ $product->name }}</p>
+  <div class="grow flex flex-col group-[.grilla]:justify-between">
+    <div
+      class="px-6 py-4 text-pretty group-[.list]:grow-0 group-[.list]:max-w-full group-[.grilla]:grow group-[.grilla]:max-w-77.5">
+      <h2 class="font-bold text-2xl mb-1 text-slate-700">{{ $product->brand->name }}</h2>
+      <p class="text-slate-500 text-base">{{ "{$product->name} - {$product->weight}" }}</p>
+    </div>
+    <livewire:_partials.add-to-cart :productId="$product->id" :offerId="$offerID" :offers="$offers" />
   </div>
-  <form action="{{ route('cart.addToCart') }}" method="POST"
-    class="px-6 pt-2 pb-2 flex flex-col justify-between gap-5">
-    @csrf
-    <input type="hidden" name="id" value="{{ $product->id }}">
-    <section class="flex justify-between items-center gap-4">
-      <div class="flex flex-col items-start justify-center">
-        @if ($offerID)
-          <p class="flex justify-star items-center gap-3">
-            @php
-              $offerApplied = $offers[$offerID];
-              $offerType = $offerApplied->offerTemplate->offerType->code;
-
-              if ($offerType === 'X_FOR_Y') {
-                  $buy = (float) $offerApplied->offerTemplate->buy_qty;
-                  $newPrice = ($product->price * ($buy - (float) $offerApplied->offerTemplate->pay_qty)) / $buy;
-              } else {
-                  $newPrice = $product->getDiscountTotal(
-                      (float) $offerApplied->offerTemplate->buy_qty,
-                      (float) $offerApplied->offerTemplate->buy_qty,
-                      (float) $offerApplied->offerTemplate->pay_qty,
-                      $offerType,
-                  );
-              }
-            @endphp
-            <span class="text-lg font-bold text-slate-700">
-              {!! "$" .
-                  number_format($product->price - $newPrice, 2, ',', '.') .
-                  ($offerType === 'X_FOR_Y' ? '<sup>c/u</sup>' : '') !!}
-            </span>
-            <span class="px-1 py-0.5 bg-amber-400 rounded-lg">
-              {{ $offerType === 'FIXED'
-                  ? '-$' . $offerApplied->offerTemplate->pay_qty
-                  : ($offerType === 'PERCENTAGE'
-                      ? '-' . $offerApplied->offerTemplate->pay_qty * 100 . '%'
-                      : $offerApplied->offerTemplate->buy_qty * 1 . 'x' . $offerApplied->offerTemplate->pay_qty * 1) }}
-            </span>
-          </p>
-        @endif
-        <p @class([
-            'text-slate-600 line-through' => $offerID,
-            'py-3 text-lg font-bold text-slate-700' => $offerID === null,
-        ])>${{ number_format($product->price, 2, ',', '.') }}</p>
-      </div>
-      <label class="w-full max-w-16 grid grid-cols-1">
-        <input type="number" name="quantity" value="1" min="1" max="{{ $product->stock }}"
-          class="ps-2 pe-0.5 py-1.5 text-base text-gray-900 bg-white rounded-md outline outline-offset-1 outline-gray-400 sm:text-sm">
-      </label>
-    </section>
-    <button type="submit"
-      class="bg-slate-700 text-white px-4 py-2 rounded-md hover:bg-slate-600 cursor-pointer">Agregar</button>
-  </form>
 </article>
