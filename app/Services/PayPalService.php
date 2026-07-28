dc9621c50f6feb2d03b83bb1ba08689ca7d73551
Gerardo <fernandez.gerardo@gmail.com>
2026-07-28 17:07:46 -0300

    remove unused components and rmove commented code

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -21,54 +21,6 @@ class PayPalService
     return $response->json()['access_token'];
   }
 
-  // public function createOrder(array $products, float $taxTotal = 0)
-  // {
-  //   $token = $this->getAccessToken();
-  //   $total = 0;
-  //   $items = collect($products)->map(function ($product) use (&$total) {
-  //     $subTotal = number_format($product['price'] * $product['quantity'], 2, '.', '');
-  //     $total += $subTotal;
-  //     return [
-  //       'name' => $product['name'],
-  //       'description' => $product['description'],
-  //       'quantity' => (string) $product['quantity'],
-  //       'unit_amount' => [
-  //         'currency_code' => 'USD',
-  //         'value' => $subTotal,
-  //       ],
-  //     ];
-  //   })->toArray();
-  //   dd(number_format($total + $taxTotal, 2, '.', ''), number_format($taxTotal, 2, '.', '') + $total, number_format($total, 2, '.', ''));
-  //   return Http::withToken($token)
-  //     ->post(config('paypal.sandbox.base_url') . "/v2/checkout/orders", [
-  //       'intent' => 'CAPTURE',
-  //       'purchase_units' => [
-  //         [
-  //           'amount' => [
-  //             'currency_code' => 'USD',
-  //             'value' => number_format($total + $taxTotal, 2, '.', ''),
-  //             'breakdown' => [
-  //               'item_total' => [
-  //                 'currency_code' => 'USD',
-  //                 'value' => number_format($total, 2, '.', ''),
-  //               ],
-  //               'tax_total' => [
-  //                 'currency_code' => 'USD',
-  //                 'value' => number_format($taxTotal, 2, '.', '')
-  //               ]
-  //             ]
-  //           ],
-  //           'items' => $items
-  //         ]
-  //       ],
-  //       'application_context' => [
-  //         'return_url' => route('paypal.success'),
-  //         'cancel_url' => route('paypal.cancel'),
-  //       ]
-  //     ])
-  //     ->json();
-  // }
-
   public function createOrder(array $products, float $taxPercent = 21)
   {
     $token = $this->getAccessToken();
